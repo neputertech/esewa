5a30466c6de44fd2acfacd85772779aaa28e73d7
Rajesh Chaudhary <135062805+rajesh-neputer@users.noreply.github.com>
2023-08-10 13:49:01 +0545

    chore: update config comments

--- src/config/esewa.php
+++ src/config/esewa.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'debug' => env('ESEWA_DEBUG', true), // set false to run on live khalti url
+    'debug' => env('ESEWA_DEBUG', true), // set false to run on live esewa url
     'website_url' => 'https://example.com', // your website url
     'scd' => env('ESEWA_SCD', 'EPAYTEST'),
 ];
