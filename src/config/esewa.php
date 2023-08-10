a3393d9b4ff175c493e65888325b93df4b7f323c
pratibimba-neputer <pratibimbakhadka@neputer.com>
2023-08-10 10:40:00 +0545

    chore: add debug config from env

--- src/config/esewa.php
+++ src/config/esewa.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'debug' => true, // set false to run on live khalti url
+    'debug' => env('ESEWA_DEBUG', true), // set false to run on live khalti url
     'website_url' => 'https://example.com', // your website url
     'scd' => env('ESEWA_SCD', 'EPAYTEST'),
 ];
